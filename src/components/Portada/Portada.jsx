0ec8aea8afef7ebdebd64d968d323281085aaf85
NicolasSaba <nicolassaba33@gmail.com>
2023-09-21 18:36:44 -0300

    release

--- src/components/Portada/Portada.jsx
+++ src/components/Portada/Portada.jsx
@@ -2,10 +2,14 @@ import "./Portada.css";
 
 const Portada = () => {
   return (
-    <div className="banner">
-      <div className="content-banner">
-        <h2>La simplicité est la clé de l'élégance.</h2>
+    <div className="portada">
+      <div className="banner">
+        <div className="content-banner">
+          <h2>La simplicité est la clé de l'élégance.</h2>
+        </div>
       </div>
+      <hr className="hr" />
+      <h2 className="subtitle">Destacados 2023</h2>
     </div>
   );
 };
